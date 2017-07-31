0ccc2561288b7026bb3be1a01f17d189212e3459
wuyiqing <cwuyiqing@gmail.com>
2017-07-31 13:53:54 +0800

    收藏模块暂时不做

--- src/renderer/components/Sidebar/collection.vue
+++ src/renderer/components/Sidebar/collection.vue
@@ -1,10 +1,13 @@
 <template>
   <div id="collection-root">
-    收藏
+    此功能暂未开放
   </div>
 </template>
 <script>
   export default {}
 </script>
-<style>
+<style lang="scss" scoped>
+  #collection-root{
+    text-align: center;
+  }
 </style>
