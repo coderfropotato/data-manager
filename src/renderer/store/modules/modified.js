184b616a37224a0ff723a6b528e7dc38596b3cc9
Guo <slightg@163.com>
2017-08-02 12:51:52 +0800

    写了一半的fileState

---
+++ src/renderer/store/modules/modified.js
@@ -0,0 +1,32 @@
+/*
+ * 文件状态功能
+ */
+
+import * as types from '@/store/mutation-types'
+
+const state = {
+  modifiedFiles: [],  // 最近变更的文件(树)
+  modifiedNum: 0 // 变更文件的数目
+}
+
+const actions = {
+  // 获取变更文件
+  getModifiedFiles ({ commit }) {
+    let files = API.getModifiedFiles()
+    commit(types.RECEIVE_MODIFIED_FILES, files)
+  }
+}
+
+const mutations = {
+  // 获取所有最近文件
+  [types.RECEIVE_MODIFIED_FILES](state, files) {
+    state.modifiedFiles = files
+    state.modifiedNum = files.length
+  }
+}
+
+export default {
+  state,
+  actions,
+  mutations
+}
