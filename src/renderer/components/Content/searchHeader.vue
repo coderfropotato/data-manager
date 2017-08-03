82ccf05980720e9848649154e23e842be4e89826
1053061407 <1053061407@qq.com>
2017-08-03 14:57:05 +0800

    搜索框以及搜索条件的实现，实现了搜索条件的增加和删除。

--- src/renderer/components/Content/searchHeader.vue
+++ src/renderer/components/Content/searchHeader.vue
@@ -43,8 +43,7 @@
         <el-col :span="20">
           <div class="grid-content">
             <div class="search-input" @click="focus">
-              <span></span>
-              <span></span>
+              <el-button v-for="item in selectedCondition" size="mini">{{item}}<i class="el-icon-circle-close" @click="deleteItem"></i></el-button>
               <input type="text" id="searchInput">
             </div>
           </div>
@@ -61,17 +60,17 @@
         <div class="line" ref="line"></div>
         <div name="类别">
           <span>类别：</span>
-          <el-button size="mini" @click="showItem" data-name="类别">公共数据</el-button>
-          <el-button size="mini" @click="showItem" data-name="类别">私有数据</el-button>
-          <el-button size="mini" @click="showItem" data-name="类别">共享数据</el-button>
+          <el-button size="mini" @click="showItem" type="text" data-name="类别">公共数据</el-button>
+          <el-button size="mini" @click="showItem" type="text" data-name="类别">私有数据</el-button>
+          <el-button size="mini" @click="showItem" type="text" data-name="类别">共享数据</el-button>
         </div>
         <div>
           <span>文件类型：</span>
-          <el-button size="mini" @click="showItem" data-name="FASTA">FASTA</el-button>
-          <el-button size="mini" @click="showItem" data-name="FASTQ">FASTQ</el-button>
-          <el-button size="mini" @click="showItem" data-name="BAM">BAM</el-button>
-          <el-button size="mini" @click="showItem" data-name="SAM">SAM</el-button>
-          <el-button size="mini" @click="showItem" data-name="VCF">VCF</el-button>
+          <el-button size="mini" @click="showItem" type="text" data-name="FASTA">FASTA</el-button>
+          <el-button size="mini" @click="showItem" type="text" data-name="FASTQ">FASTQ</el-button>
+          <el-button size="mini" @click="showItem" type="text" data-name="BAM">BAM</el-button>
+          <el-button size="mini" @click="showItem" type="text" data-name="SAM">SAM</el-button>
+          <el-button size="mini" @click="showItem" type="text" data-name="VCF">VCF</el-button>
         </div>
       </div>
     </div>
@@ -86,16 +85,9 @@
       return {
         searchValue: '',
         show: true,
-        condition: {
-          publicData: '公共数据',
-          privateData: '私有数据',
-          sharedData: '共享数据',
-          FASTA: 'FASTA',
-          FASTQ: 'FASTQ',
-          BAM: 'BAM',
-          SAM: 'SAM',
-          VCF: 'VCF'
-        },
+        condition: ['公共数据', '私有数据', '共享数据', 'FASTA', 'FASTQ', 'BAM', 'SAM', 'VCF'], // 列出的条件
+        selectedCondition: [],
+        // 用户选择的条件，通过v-for遍历这个集合以显示用户点击的条件。输入框条件的增加删除，也是通过向这个集合添加或者删除元素来实现
         i: 0
       }
     },
@@ -122,17 +114,38 @@
       },
       showItem (e) {
         let text = e.target.innerText
-        console.log(e.target)
-        let amount = $('.search-input').children('span').length
-        if (this.i < amount) {
+        let count = 0
+        let length = this.selectedCondition.length
+        // 如果用户还未选择条件，则直接添加
+        if (length === 0) {
           if (text === '公共数据' || text === '私有数据' || text === '共享数据') {
-            $('.search-input').children('span').eq(this.i).text('类别：' + text)
+            this.selectedCondition.push('类别：' + text)
           } else {
-            $('.search-input').children('span').eq(this.i).text('文件类型：' + text)
+            this.selectedCondition.push('文件类型：' + text)
           }
-          $('.search-input').children('span').eq(this.i).css('border', '1px solid #000')
-          this.i++
+        } else { // 如果已经有选择的条件，判断用户点击的条件是否重复
+          for (this.i = 0; this.i < length; this.i++) {
+            if (this.selectedCondition[this.i].indexOf(text) !== -1) {
+              count++
+            }
+          } // 如果没有重复，则向selectedCondition添加元素
+          if (count === 0) {
+            if (text === '公共数据' || text === '私有数据' || text === '共享数据') {
+              this.selectedCondition.push('类别：' + text)
+            } else {
+              this.selectedCondition.push('文件类型：' + text)
+            }
+          }
         }
+      },
+      deleteItem (e) {
+        let text = $(e.target).parent().parent().text()
+        for (this.i = 0; this.i < this.selectedCondition.length; this.i++) {
+          if (text === this.selectedCondition[this.i]) {
+            var index = this.i
+          }
+        }
+        this.selectedCondition.splice(index, 1)
       },
       focus () {
         document.querySelector('#searchInput').focus()
@@ -160,34 +173,27 @@
       background-color: #48576a;
       margin: 1em auto;
     }
-    .el-button {
-      border-color: #FFFFFF;
-    }
   }
-
   .header{
     .search-input{
+        .el-button {
+          border-radius: 12px;
+          margin-left: 0.3em;
+          font-size: 1em;
+        }
       margin-top: 0.7em;
-      span {
-        border-radius: 12px;
-        padding: 1px 3px 1px;
-        margin-left: 0.3em;
-        font-size: 0.7em;
-        position: relative;
-        bottom: 1.3em;
-      }
       input{
         position: relative;
-        bottom: 1em;
-        left: 0.2em;
+        bottom: 0.5em;
+        left: 0.5em;
         background-color: #eef1f6;
         height: 1.5em;
         outline: none;
         border: none;
         vertical-align: baseline;
-        font-size: 0.8em;
+        font-size: 1em;
       }
-      height: 1.5em;
+      height: 2em;
       border: 1px solid #000;
       border-radius: 15px;
     }
