a4e04231fc24d93d0b761cc57c6d076255c3cbc7
wuyiqing <cwuyiqing@gmail.com>
2017-08-11 11:53:59 +0800

    修复异步操作错误

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -23,55 +23,55 @@
 //        }
         // 向后台请求创建智能视图的限制条件
         this.$store.dispatch('getSearchConditions').then(() => {
+          // 获取导入 Excel 表格的可选择磁盘
           this.$store.dispatch('openFile').then(() => {
             // 向后台请求创建智能视图的限制条件
             this.$store.dispatch('getSearchConditions').then(() => {
-              // 获取导入 Excel 表格的可选择磁盘
               this.$store.dispatch('getImportTargetDisks')
             })
-
           })
         })
-        // 接受与后台交互的 API 的错误提示
-        bus.$on('error', () => {
-          bus.$emit('loading-end')
-          this.$notify({
-            type: 'error',
-            message: '数据读取错误，请重试或尝试重启软件',
-            duration: 0
-          })
+      })
+      // 接受与后台交互的 API 的错误提示
+      bus.$on('error', () => {
+        bus.$emit('loading-end')
+        this.$notify({
+          type: 'error',
+          message: '数据读取错误，请重试或尝试重启软件',
+          duration: 0
         })
-        /*
-         * 通过主进程在窗口之间传递数据
-         * @state 说明 action/mutation 调用方式和接口
-         * @data 窗口传递的数据
-         */
-        ipcRenderer.on('change-data', (event, call, data) => {
-          // 调用action
-          if (call.mode === 'action') {
-            this.$store.dispatch(call.API, data)
-          }
-          // 调用mutation
-          if (call.mode === 'mutation') {
-            this.$store.dispatch(call, data)
-          }
-        })
+      })
+      /*
+       * 通过主进程在窗口之间传递数据
+       * @state 说明 action/mutation 调用方式和接口
+       * @data 窗口传递的数据
+       */
+      ipcRenderer.on('change-data', (event, call, data) => {
+        // 调用action
+        if (call.mode === 'action') {
+          this.$store.dispatch(call.API, data)
+        }
+        // 调用mutation
+        if (call.mode === 'mutation') {
+          this.$store.dispatch(call, data)
+        }
+      })
 
-        // 禁用浏览器默认拖拽事件，防止用户拖拽的文件被打开
-        document.addEventListener('drop', e => {
-          e.preventDefault()
-        }, false)
-        document.addEventListener('dragenter', e => {
-          e.preventDefault()
-        }, false)
-        document.addEventListener('dragleave', e => {
-          e.preventDefault()
-        }, false)
-        document.addEventListener('dragover', e => {
-          e.preventDefault()
-        }, false)
-      }
+      // 禁用浏览器默认拖拽事件，防止用户拖拽的文件被打开
+      document.addEventListener('drop', e => {
+        e.preventDefault()
+      }, false)
+      document.addEventListener('dragenter', e => {
+        e.preventDefault()
+      }, false)
+      document.addEventListener('dragleave', e => {
+        e.preventDefault()
+      }, false)
+      document.addEventListener('dragover', e => {
+        e.preventDefault()
+      }, false)
     }
+  }
 </script>
 
 <style lang="scss">
