6d71085b0817366ee0cb947990b5a39132101237
yangwd <yangwd@gooalgene.com>
2017-10-27 15:43:31 +0800

    modify mainwin

--- src/main/index.js
+++ src/main/index.js
@@ -1,11 +1,9 @@
 'use strict'
 
-import { app, BrowserWindow, ipcMain, dialog } from 'electron'
-
+import { app, BrowserWindow, ipcMain, dialog, Tray ,Menu,nativeImage } from 'electron'
 if (process.env.NODE_ENV !== 'development') {
   global.__static = require('path').join(__dirname, '/static').replace(/\\/g, '\\\\')
 }
-
 let mainWindow
 const winURL = process.env.NODE_ENV === 'development'
   ? `http://localhost:9080/#`
@@ -21,7 +19,6 @@ function createWindow() {
     // minWidth: 800,
     height: 800,
     width: 1200,
-    // frame:false,
     useContentSize: true
   })
   mainWindow.setMinimumSize(1200, 800)
@@ -30,6 +27,28 @@ function createWindow() {
   mainWindow.on('closed', () => {
     mainWindow = null
   })
+
+  // //系统托盘图标
+  // let tray = new Tray(icon)
+  // //tip
+  // tray.setToolTip('gooalgene-data-manager')
+  // //双击icon 显示窗口
+  // tray.on('double-click', () => {
+  //   mainWindow.show()
+  //   mainWindow.focus()
+  // });
+  // tray.on('right-click', () => {
+  //   let contextMenu = Menu.buildFromTemplate([
+  //     {
+  //       label: '退出系统',
+  //       click: function () {
+  //         tray.destroy()
+  //         app.quit()
+  //       }
+  //     }
+  //   ]);
+  //   tray.setContextMenu(contextMenu);
+  // })
 }
 
 /*
@@ -82,27 +101,32 @@ app.on('ready', createWindow)
 
 //关闭app
 ipcMain.on('window-all-closed', () => {
-  dialog.showMessageBox({
-    type:"question",
-    title:"提示信息",
-    buttons :['确定','cancel'],
-    message:'点击"确定"退出程序'
-  },function(index){
-    // if(process.platform!=='darwin')
-    if(index==0) app.quit()
-  })
+  // dialog.showMessageBox({
+  //   type: "question",
+  //   title: "提示信息",
+  //   buttons: ['确定', 'cancel'],
+  //   message: '点击"确定"退出程序'
+  // }, function (index) {
+  //   // if(process.platform!=='darwin')
+  //   if (index == 0) app.quit()
+  // })
+  mainWindow.minimize();
+  mainWindow.setSkipTaskbar(true)
 });
 //小化
 ipcMain.on('hide-window', (ev) => {
   mainWindow.minimize();
+  mainWindow.setSkipTaskbar(false)
 });
 //最大化
 ipcMain.on('show-window', () => {
   mainWindow.maximize();
+  mainWindow.setSkipTaskbar(false)
 });
 //还原
 ipcMain.on('orignal-window', () => {
   mainWindow.unmaximize();
+  mainWindow.setSkipTaskbar(false)
 });
 
 app.on('activate', () => {
