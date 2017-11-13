b4502452b02681bb51e9c539504ad8d572f9f218
张浩浩 <hyacinth.hao@foxmail.com>
2017-11-14 01:51:53 +0800

    添加启动时开启Python子线程的代码（已自测：如果路径配置正确是可以启动的。需改进：启动失败时也会有打印回显）

--- src/main/index.js
+++ src/main/index.js
@@ -149,3 +149,56 @@ app.on('activate', () => {
     createWindow()
   }
 })
+
+
+/*************************************************************
+ * py process
+ *************************************************************/
+const path = require('path')
+const PY_DIST_FOLDER = 'dist'
+const PY_FOLDER = 'backend'
+const PY_MODULE = 'backend' // without .py suffix
+const PY_PORT = 4242
+
+let pyProc = null
+let pyPort = null
+
+const guessPackaged = () => {
+  const fullPath = path.join(__dirname, PY_DIST_FOLDER)
+  return require('fs').existsSync(fullPath)
+}
+
+const getScriptPath = () => {
+  if (!guessPackaged()) {
+    return path.join(__dirname, PY_FOLDER, PY_MODULE + '.py')
+  }
+  if (process.platform === 'win32') {
+    return path.join(__dirname, PY_DIST_FOLDER, PY_MODULE, PY_MODULE + '.exe')
+  }
+  return path.join(__dirname, PY_DIST_FOLDER, PY_MODULE, PY_MODULE)
+}
+
+const createPyProc = () => {
+  let script = getScriptPath()
+  let port = '' + PY_PORT
+
+  if (guessPackaged()) {
+    pyProc = require('child_process').execFile(script, [port])
+  } else {
+    pyProc = require('child_process').spawn('python', [script, port])
+  }
+ 
+  if (pyProc != null) {
+    console.log(pyProc)
+    console.log('child process success on port ' + port)  // 启动失败也会打印
+  }
+}
+
+const exitPyProc = () => {
+  pyProc.kill()
+  pyProc = null
+  pyPort = null
+}
+
+app.on('ready', createPyProc)
+app.on('will-quit', exitPyProc)
