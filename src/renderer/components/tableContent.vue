abb51b8d65ca58f83715a96ef5e97493b3f69c84
yangwd <yangwd@gooalgene.com>
2017-12-07 10:32:59 +0800

    modified css

--- src/renderer/components/tableContent.vue
+++ src/renderer/components/tableContent.vue
@@ -4,16 +4,16 @@
       <el-table  ref="table"  @row-dblclick="dbClick" @selection-change="handleSelectionChange" 
       @cell-click="cellClick"
       :height="tableHeight" :data="loadData" stripestyle="width: 100%">
-        <el-table-column type="selection"></el-table-column>
-        <el-table-column  prop="isdir" label="选择">
+        <el-table-column  type="selection"></el-table-column>
+        <el-table-column prop="isdir" label="选择">
           <template scope="scope">
             <img v-if="!scope.row.isdir" src="../assets/images/single.png"/>
             <img v-else src="../assets/images/dir.png"/>
           </template>
         </el-table-column>
-        <el-table-column sortable prop="filename" label="文件名"></el-table-column>
-        <el-table-column sortable prop="ctime" label="创建时间"></el-table-column>
-        <el-table-column sortable prop="size" label="文件大小">
+        <el-table-column  sortable prop="filename" label="文件名"></el-table-column>
+        <el-table-column  sortable prop="ctime" label="创建时间"></el-table-column>
+        <el-table-column  sortable prop="size" label="文件大小">
           <template scope="scope">
             <span>{{scope.row.size | reverseSize}}</span>
           </template>
