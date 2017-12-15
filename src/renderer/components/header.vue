ced22f1515ee17bdddb5a563a96df58f53ce7280
yangwd <yangwd@gooalgene.com>
2017-12-15 15:51:58 +0800

    mock logo clicked jumpto root

--- src/renderer/components/header.vue
+++ src/renderer/components/header.vue
@@ -2,8 +2,9 @@
   <div id="header">
       <div class="header-context">
         <div class="left">
+          <!-- @click="root" -->
           <img src="../assets/images/logo.png" alt="">
-            <span>数据管理系统</span>
+          <span>数据管理系统</span>
         </div>
         <ol class="right">
             <li @click="hide" title="最小化"></li>
@@ -18,15 +19,19 @@
 <script>
 export default {
   data() {
-    return {
-    };
+    return {};
   },
   methods: {
+    root() {
+      this.$router.push("./root");
+      this.$store.dispatch('setGlobalNavIndex',0);
+      this.$store.dispatch('setRouteStatus','root');
+    },
     hide() {
       this.$electron.ipcRenderer.send("hide-window");
     },
     zoom() {
-      this.$electron.ipcRenderer.send('change-window');
+      this.$electron.ipcRenderer.send("change-window");
     },
     close() {
       this.$electron.ipcRenderer.send("window-all-closed");
@@ -52,17 +57,19 @@ export default {
     .left {
       display: flex;
       flex: 1;
-      -webkit-app-region: drag;
       img {
         width: 40px;
         height: 40px;
         margin-right: 20px;
         display: block;
         margin-top: 13px;
+        // cursor: pointer;
       }
       span {
         color: #fff;
         font-size: 18px;
+        flex: 1;
+        -webkit-app-region: drag;
       }
     }
     .right {
@@ -76,9 +83,9 @@ export default {
         background-size: 16px 16px;
         background-repeat: no-repeat;
         background-position: center;
-        transition: .1s all ease;
+        transition: 0.1s all ease;
         cursor: pointer;
-        opacity: .8;
+        opacity: 0.8;
         margin-top: 3px;
         &:first-child {
           background-image: url("../assets/images/hide.png");
