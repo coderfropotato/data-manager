1eaa2d3975d798cbffeb59834a0a3645686e4b24
yangwd <yangwd@gooalgene.com>
2018-01-10 15:15:32 +0800

    add backend bat

--- backend.bat
+++ backend.bat
@@ -8,34 +8,19 @@ git checkout with_tornado
 echo "down backend code success"
 virtualenv venv
 call .\venv\Scripts\activate.bat
-pip install -i https://pypi.tuna.tsinghua.edu.cn/simple asn1crypto==0.22.0
-pip install -i https://pypi.tuna.tsinghua.edu.cn/simple bcrypt==3.1.3
+
 pip install -i https://pypi.tuna.tsinghua.edu.cn/simple certifi==2017.7.27.1
-pip install -i https://pypi.tuna.tsinghua.edu.cn/simple cffi==1.10.0
 pip install -i https://pypi.tuna.tsinghua.edu.cn/simple chardet==3.0.4
-pip install -i https://pypi.tuna.tsinghua.edu.cn/simple cryptography==2.0.2
 pip install -i https://pypi.tuna.tsinghua.edu.cn/simple future==0.16.0
-pip install -i https://pypi.tuna.tsinghua.edu.cn/simple idna==2.5
-pip install -i https://pypi.tuna.tsinghua.edu.cn/simple iso8601==0.1.12
-pip install -i https://pypi.tuna.tsinghua.edu.cn/simple ntlm-auth==1.0.6
 pip install -i https://pypi.tuna.tsinghua.edu.cn/simple paramiko==2.2.1
 pip install -i https://pypi.tuna.tsinghua.edu.cn/simple psutil==5.4.0
-pip install -i https://pypi.tuna.tsinghua.edu.cn/simple pyasn1==0.3.1
-pip install -i https://pypi.tuna.tsinghua.edu.cn/simple pycparser==2.18
-pip install -i https://pypi.tuna.tsinghua.edu.cn/simple PyNaCl==1.1.2
-pip install -i https://pypi.tuna.tsinghua.edu.cn/simple PyYAML==3.12
-pip install -i https://pypi.tuna.tsinghua.edu.cn/simple pyzmq==16.0.2
 pip install -i https://pypi.tuna.tsinghua.edu.cn/simple requests==2.18.4
-pip install -i https://pypi.tuna.tsinghua.edu.cn/simple requests-ntlm==1.1.0
 pip install -i https://pypi.tuna.tsinghua.edu.cn/simple serial==0.0.15
 pip install -i https://pypi.tuna.tsinghua.edu.cn/simple six==1.10.0
 pip install -i https://pypi.tuna.tsinghua.edu.cn/simple tinydb==3.3.1
 pip install -i https://pypi.tuna.tsinghua.edu.cn/simple urllib3==1.22
 pip install -i https://pypi.tuna.tsinghua.edu.cn/simple WMI==1.4.9
 pip install -i https://pypi.tuna.tsinghua.edu.cn/simple wrapcache==1.0.8
-pip install -i https://pypi.tuna.tsinghua.edu.cn/simple xlrd==1.0.0
-pip install -i https://pypi.tuna.tsinghua.edu.cn/simple xlwt==1.2.0
-pip install -i https://pypi.tuna.tsinghua.edu.cn/simple xmltodict==0.11.0
 pip install -i https://pypi.tuna.tsinghua.edu.cn/simple numpy==1.13.1
 pip install -i https://pypi.tuna.tsinghua.edu.cn/simple scipy==1.0.0
 pip install -i https://pypi.tuna.tsinghua.edu.cn/simple pandas==0.21.0
