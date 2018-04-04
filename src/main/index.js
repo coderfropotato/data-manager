3028cd15bf91f3cd78e0453d71c211746e50c335
yangwd <yangwd@gooalgene.com>
2018-04-04 15:13:48 +0800

    Add : electron-updater

--- src/main/index.js
+++ src/main/index.js
@@ -10,6 +10,7 @@ import {
   nativeImage
 } from 'electron'
 import { create } from 'domain';
+
 if (process.env.NODE_ENV !== 'development') {
   global.__static = require('path').join(__dirname, '/static').replace(/\\/g, '\\\\')
 }
@@ -42,6 +43,7 @@ function createWindow() {
   mainWindow.setMinimumSize(1200, 860)
   mainWindow.loadURL(winURL)
   mainWindow.setMenu(null)
+  // mainWindow.webContents.openDevTools();
   mainWindow.on('closed', () => {
     mainWindow = null;
     tray = null;
@@ -336,3 +338,52 @@ app.on('activate', () => {
     createWindow()
   }
 });
+
+
+/**
+ * auto updater
+ * joke <277637411@qq.com>
+ */
+function updateHandle(){
+  let message={
+    error:'检查更新出错',
+    checking:'正在检查更新……',
+    updateAva:'检测到新版本，正在下载……',
+    updateNotAva:'现在使用的就是最新版本，不用更新',
+  };
+  const os = require('os');
+  autoUpdater.setFeedURL('https://github.com/YeahPotato/electron-release-packages.git');
+  autoUpdater.on('error', function(error){
+    sendUpdateMessage(message.error)
+  });
+  autoUpdater.on('checking-for-update', function() {
+    sendUpdateMessage(message.checking)
+  });
+  autoUpdater.on('update-available', function(info) {
+      sendUpdateMessage(message.updateAva)
+  });
+  autoUpdater.on('update-not-available', function(info) {
+      sendUpdateMessage(message.updateNotAva)
+  });
+  
+  // 更新下载进度事件
+  autoUpdater.on('download-progress', function(progressObj) {
+      mainWindow.webContents.send('downloadProgress', progressObj)
+  })
+  autoUpdater.on('update-downloaded',  function (event, releaseNotes, releaseName, releaseDate, updateUrl, quitAndUpdate) {
+      ipcMain.on('isUpdateNow', (e, arg) => {
+          //some code here to handle event
+          autoUpdater.quitAndInstall();
+      })
+      mainWindow.webContents.send('isUpdateNow')
+  });
+  
+  //执行自动更新检查
+  autoUpdater.checkForUpdates();
+}
+
+// 通过main进程发送事件给renderer进程，提示更新信息
+// mainWindow = new BrowserWindow()
+function sendUpdateMessage(text){
+  mainWindow.webContents.send('message', text)
+}
