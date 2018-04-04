e2fe073d71358be86364deb338e9c359399644b3
yangwd <yangwd@gooalgene.com>
2018-04-04 15:14:10 +0800

    Updata : formate

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -1,38 +1,30 @@
-import Vue from 'vue'
-// import Raven from 'raven-js'
-// import RavenVue from 'raven-js/plugins/vue'
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-default/index.css'
-import App from './App'
-import router from './router'
-import store from './store'
-import Header from './components/header'
-import Bottom from './components/bottom'
-import TableContent from './components/tableContent'
-import NavBar from './components/navbar'
-import EditDom from './components/edit'
-let tools = require('@/assets/JS/tools')
+import Vue from 'vue';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-default/index.css';
+import App from './App';
+import router from './router';
+import store from './store';
+import Header from './components/header';
+import Bottom from './components/bottom';
+import TableContent from './components/tableContent';
+import NavBar from './components/navbar';
+import EditDom from './components/edit';
+
+
+let tools = require('@/assets/JS/tools');
 Vue.prototype.tools = tools;
 Vue.config.productionTip = false
 Vue.use(ElementUI)
-if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
-// Sentry 错误报告
-// Raven
-//     .config('https://f3c58ec9eb8f47b584c42bed2771ea74@sentry.io/195260')
-//     .addPlugin(RavenVue, Vue)
-//     .install()
-
-/* eslint-disable no-new */
-
+if (!process.env.IS_WEB) Vue.use(require('vue-electron'));
 
 //import global components
-Vue.component('Header', Header)
-Vue.component('Bottom', Bottom)
-Vue.component('myTable', TableContent)
-Vue.component('myNav', NavBar)
-Vue.component('EditDom', EditDom)
+Vue.component('Header', Header);
+Vue.component('Bottom', Bottom);
+Vue.component('myTable', TableContent);
+Vue.component('myNav', NavBar);
+Vue.component('EditDom', EditDom);
 
-//filter
+//global filter
 Vue.filter('reverseSize', (val) => {
   if (val < 1024) {
     return val + 'B';
@@ -45,7 +37,7 @@ Vue.filter('reverseSize', (val) => {
   } else if (val >= Math.pow(1024, 4) && val < Math.pow(1024, 5)) {
     return Math.round(val / 1024 / 1024 / 1024 * 100) / 100 + 'T';
   }
-})
+});
 // 时间戳转换
 Vue.filter('reverseTime', (val) => {
   let time = Math.round(val * 1000);
@@ -57,12 +49,13 @@ Vue.filter('reverseTime', (val) => {
   let mm = oDate.getMinutes();
   let s = oDate.getSeconds();
   return y + '-' + addZero(m) + '-' + addZero(d) + ' ' + addZero(h) + ':' + addZero(mm) ;
-})
+});
 
 function addZero(m) {
   return m < 10 ? '0' + m : m
 }
 
+// mount
 new Vue({
   router,
   store,
